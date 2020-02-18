1aee460a933f079f9305213c0e734d12f0075039
Teemu Lemberg <teemulemberg@Teemus-MacBook-Pro.local>
2020-02-18 22:09:23 +0200

    Adding lots of files based on instruction video. More relevant commits comming in the future

--- website/views.py
+++ website/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('<h1> Website Homepage</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1> Website About page</h1>')
 
 # Create your views here.
