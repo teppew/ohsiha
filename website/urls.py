61c05a23842b4550d812541dc913001881b41bcf
teppew <teemu.lemberg@tuni.fi>
2020-03-28 00:41:35 +0200

    Added features related to sentiment analysis

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
+from django.conf.urls import url
+from django.conf.urls.defaults import *
 
 urlpatterns = [
     path('', views.home, name='website-home'),
+    path('search/', views.home, name='website-home'),
     path('about/', views.about, name='website-about'),
+    path(r'', include('gmapi.urls.media')),
 ]
+
+
+# http://127.0.0.1:8000/search/?q=sorona
--- website/views.py
+++ website/views.py
@@ -1,31 +1,71 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
-
-# Something very random to text how the query should work
-posts = [
-    {
-        'author': 'Teemu',
-        'title': 'First post',
-        'date': 'Feb 19, 2020',
-        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
-    },
-    {
-        'author': 'Pertti',
-        'title': 'Second post',
-        'date': 'Feb 20, 2020',
-        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
-    }
-]
+import tweepy, csv, sys, re
+from website import twitterAPI
+from website import  GoogleAPI
 
 
+# This is supposed to make the homescreen blank before submitting any word to the search box
 def home(request):
-    context = {
-        'posts' : posts
-    }
 
-    # Takes the html file from templates folder which is inside the website project folder
-    return render(request, 'website/home.html', context)
+    if request.GET:
+            hashtag = request.GET['q']
+
+            # polaritys = [Positive, Negative, Neutral, Hashtag]
+            # Allteets = [location, username , created, text]
+            [polaritys, alltweets] = twitterAPI.twitter_streamer(hashtag, 200)
+            show_content = True
+            context = {'polaritys': polaritys, 'show_content': show_content}
+
+            # Takes the html file from templates folder which is inside the website project folder
+            return render(request, 'website/home.html', context)
+    else:
+        show_content = False
+        context = {'show_content': show_content}
+        return render(request, 'website/home.html', context)
 
 
 def about(request):
     return render(request, 'website/about.html')
+
+
+# When submitting a word to the search box this function should
+# return the word back to home.html as a 'context'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+    count = 20
+    hashtag = "persvako"
+    context = {'hashtag': hashtag}
+    hashtag = request.GET.get(search)
+
+    return render(request, 'website/home.html', context)
+
+    
+'''
+
+
+
