c30afa87b86325bd4d2077bb8dcfb8cb2648b2dc
teppew <teemu.lemberg@tuni.fi>
2020-04-01 22:32:39 +0300

    mmm

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    path('', views.home, name='website-home'),
+    path('', views.about, name='website-about'),
     path('search/', views.home, name='website-home'),
     path('about/', views.about, name='website-about'),
 ]
